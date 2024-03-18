37b352ea50c9ed62277f7a4a037af66109158c1c
seeifsalem <mohamedseifsalem@gmail.com>
2024-03-18 19:54:14 +0100

    minor changes

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def main():
     st.markdown('[Author email](mailto:ethanujohnston@gmail.com)')
     use_gpt_4 = st.checkbox("Use GPT-4 for the final prompt (STRONGLY recommended, requires GPT-4 API access - progress bar will appear to get stuck as GPT-4 is slow)", value=True)
     find_clusters = st.checkbox('Find optimal clusters (experimental, could save on token usage)', value=False)
-    st.sidebar.markdown('# Made by: [Ethan](https://github.com/e-johnstonn)')
-    st.sidebar.markdown('# Git link: [Docsummarizer](https://github.com/e-johnstonn/docsummarizer)')
+    st.sidebar.markdown('# Made by: [Seif](https://github.com/seeifsalem)')
+    st.sidebar.markdown('# Course: [Lean Startup Academy](https://www.kickbox.academy/)')
     st.sidebar.markdown("""<small>It's always good practice to verify that a website is safe before giving it your API key. 
                         This site is open source, so you can check the code yourself, or run the streamlit app locally.</small>""", unsafe_allow_html=True)
 
